4e911523cb897e0c38eb590d0a3c14af0de66043
Leche <lechedeburro420@gmail.com>
2022-12-30 15:42:40 -0500

    innit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import Header from "./Header";
 
 export default function Home() {
   return (
@@ -14,110 +13,45 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <div className="body bg-[#343542] w-screen h-full">
+        {/* Header */}
+        <Header />
+        {/* ChatLog */}
+        <div className="flex flex-col space-y-2 p-2">
+          <div className="chat-box-left">Hey I'm a robot BEEP BOOP</div>
+          <div className="chat-box-left">
+            I'm a robot and I can do whatever you waht, ask me something go
+            ahead, ask me ask me ask me
+          </div>
+          <div className="chat-box-right">
+            ullamcorper, sollicitudin leo vitae, egestas elit. Nulla tristique
+            sed lectus eu eleifend. Morbi facilisis nunc turpis, vel iaculis
+            justo viverra quis. Pellentesque feugiat velit sed risus interdum,
+            vel maximus purus gravida.
+          </div>
+          <div className="chat-box-right">
+            eros urna rhoncus risus, vitae ultricies nibh diam sit amet tortor.
+            Sed congue justo eget consectetur fermentum. Nam commodo velit sed
+            cursus blandit.
+          </div>
+          <div className="chat-box-left">
+            Fusce pretium nisl a luctus dapibus. Vivamus efficitur tellus ac
+            dolor tincidunt tempus. Vestibulum ante ipsum primis in faucibus
+            orci luctus et ultrices posuere cubilia curae; In vel justo lectus.
+            Vestibulum eget odio nisi. Curabitur ac mi sed ante luctus dictum.
+            Cras et auctor nisi, sit amet malesuada dolor.
+          </div>
+          <div className="chat-box-right">
+            Bing Bong
+          </div>
+          <div className="chat-box-left">
+            Fusce pretium nisl a luctus dapibus. Vivamus efficitur tellus ac
+            dolor tincidunt tempus. Vestibulum ante ipsum primis in faucibus
+            orci luctus et ultrices posuere cubilia curae;
           </div>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        {/* ChatInput */}
+      </div>
     </>
-  )
+  );
 }
